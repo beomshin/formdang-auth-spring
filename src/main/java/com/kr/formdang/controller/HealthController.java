cf2b4837d55f48072cb7a9a88301d058598e2869
beomshin.kim <chzh859@naver.com>
2023-11-21 21:41:49 +0900

    # 헬스 체크

---
+++ src/main/java/com/kr/formdang/controller/HealthController.java
@@ -0,0 +1,20 @@
+package com.kr.formdang.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class HealthController {
+
+
+    @GetMapping("/api/public/health")
+    public ResponseEntity health() {
+        log.debug("[헬스체크 성공] ==============> ");
+        return ResponseEntity.ok().build();
+    }
+}
