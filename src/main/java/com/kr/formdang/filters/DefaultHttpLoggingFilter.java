3cb7e1e72e414773059d62e44eba7e11d6fdce53
beomshin.kim <chzh859@naver.com>
2023-12-16 23:58:50 +0900

    # Cors 테스트

--- src/main/java/com/kr/formdang/filters/DefaultHttpLoggingFilter.java
+++ src/main/java/com/kr/formdang/filters/DefaultHttpLoggingFilter.java
@@ -61,11 +61,6 @@ public class DefaultHttpLoggingFilter extends OncePerRequestFilter {
     stopWatch.stop(); // watch stop
 
     response.setHeader("Access-Control-Allow-Origin", "*");
-
-    for (String header : response.getHeaderNames()) {
-      log.info("[헤더] : {}", response.getHeader(header));
-    }
-
     log.info("Returned status=[{}] in [{}]ms, charset=[{}]", response.getStatus(), stopWatch.getTotalTimeMillis(), response.getCharacterEncoding());
   }
 
