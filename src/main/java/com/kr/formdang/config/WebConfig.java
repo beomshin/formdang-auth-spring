ea5996162d375478976373f7fd73191c856a3e4d
beomshin.kim <chzh859@naver.com>
2024-10-03 20:39:30 +0900

    # 공통로그 설정, xml -> json

--- src/main/java/com/kr/formdang/config/WebConfig.java
+++ src/main/java/com/kr/formdang/config/WebConfig.java
@@ -1,9 +1,16 @@
 package com.kr.formdang.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.xml.AbstractXmlHttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -18,6 +25,26 @@ public class WebConfig implements WebMvcConfigurer {
                 .exposedHeaders("Content-Disposition")
                 .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // XML 관련 HttpMessageConverter 의 우선순위를 최하위로 낮추는 메소드
+        reorderXmlConvertersToEnd(converters);
+    }
+
+    private void reorderXmlConvertersToEnd(List<HttpMessageConverter<?>> converters) {
+        List<HttpMessageConverter<?>> xml = new ArrayList<>();
+        for (Iterator<HttpMessageConverter<?>> iterator =
+             converters.iterator(); iterator.hasNext();) {
+            HttpMessageConverter<?> converter = iterator.next();
+            if ((converter instanceof AbstractXmlHttpMessageConverter)
+                    || (converter instanceof MappingJackson2XmlHttpMessageConverter)) {
+                xml.add(converter);
+                iterator.remove();
+            }
+        }
+        converters.addAll(xml);
     }
 
 }
